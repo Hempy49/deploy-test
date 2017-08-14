83597a3f50eeca80caeb66e80d1f9da933a17e45
Simon Hempenstall <simonjhemp@hotmail.com>
2017-08-14 17:20:11 +0100

    Attempt 1

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/app'));
-app.set('view engine', 'ejs');
+// app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (request, response) {
-  response.render('pages/index');
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('views/index.html');
 });
 
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
